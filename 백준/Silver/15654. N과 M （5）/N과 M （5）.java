e7f2a8e0fb5c203accff482f12ace02114902678
Mingyu Joung <samu9nai@gmail.com>
2026-04-20 12:50:11 +0900

    [Silver III] Title: N과 M (5), Time: 220 ms, Memory: 31528 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static int[] arr;
+    static int[] selected;
+    static boolean[] visited;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        selected = new int[m];
+        visited = new boolean[n];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int sidx) {
+        if (sidx == m) {
+            for (int x : selected) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+
+            selected[sidx] = arr[i];
+            visited[i] = true;
+            dfs(sidx + 1);
+            visited[i] = false;
+        }
+    }
+}
