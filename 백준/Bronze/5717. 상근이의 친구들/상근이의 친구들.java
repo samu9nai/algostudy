6f4158d5dfbe1f6d4006626e728e0816ae582d14
Mingyu Joung <samu9nai@gmail.com>
2026-04-21 09:33:49 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            if (n == 0 && m == 0) break;
+            System.out.println(n + m);
+        }
+    }
+}
