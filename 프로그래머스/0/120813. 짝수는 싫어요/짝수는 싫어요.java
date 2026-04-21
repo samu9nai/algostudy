d894898be627a711de5cefdc8fc7cc8494307e23
Mingyu Joung <samu9nai@gmail.com>
2026-04-21 10:07:46 +0900

    [level 0] Title: 짝수는 싫어요, Time: 3.33 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i += 2){
+            answer.add(i);
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
