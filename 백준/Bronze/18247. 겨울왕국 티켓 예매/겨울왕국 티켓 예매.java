e7e0de4ee5f14598d10f468d0c9eee706d7584c0
Mingyu Joung <samu9nai@gmail.com>
2026-04-22 09:26:25 +0900

    [Bronze III] Title: 겨울왕국 티켓 예매, Time: 64 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Bronze/18247. 겨울왕국 티켓 예매/겨울왕국 티켓 예매.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            int pos = -1;
+
+            if (n >= 12 && m >= 4) {
+                pos = 11 * m + 4;
+            }
+
+            System.out.println(pos);
+        }
+    }
+}
