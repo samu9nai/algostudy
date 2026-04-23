ee2f5559a55180c26a67521d402aa912026e4c87
Mingyu Joung <samu9nai@gmail.com>
2026-04-23 10:56:28 +0900

    [Silver III] Title: 수열과 쿼리 38, Time: 684 ms, Memory: 132548 KB -BaekjoonHub

--- 백준/Silver/18917. 수열과 쿼리 38/수열과 쿼리 38.java
+++ 백준/Silver/18917. 수열과 쿼리 38/수열과 쿼리 38.java
@@ -1,12 +1,35 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int m = Integer.parseInt(br.readLine());
+
+        long sum = 0;
+        long xor = 0;
+
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+            if (cmd == 1) {
+                int x = Integer.parseInt(st.nextToken());
+                sum += x;
+                xor = xor ^ x;
+            } else if (cmd == 2) {
+                int x = Integer.parseInt(st.nextToken());
+                sum -= x;
+                xor = xor ^ x;
+            } else if (cmd == 3) {
+                sb.append(sum).append("\n");
+            } else if (cmd == 4) {
+                sb.append(xor).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
