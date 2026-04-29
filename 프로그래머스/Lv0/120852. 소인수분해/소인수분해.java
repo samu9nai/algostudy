3e385b9acc26591e911cf858f39206ef72fcb0fb
Mingyu Joung <samu9nai@gmail.com>
2026-04-29 10:22:43 +0900

    [level 0] Title: 소인수분해, Time: 3.68 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) { 
+                list.add(i); 
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
