168cb09dcac1d35d6316659426e643ab277b4f2b
Mingyu Joung <samu9nai@gmail.com>
2026-04-30 10:44:16 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/Lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int cnt = 0;
+
+        for (String str1 : s1) {
+            for (String str2 : s2) {
+                if (str1.equals(str2)) {
+                    cnt++;
+                }
+            }
+        }
+        
+        return cnt;
+    }
+}
