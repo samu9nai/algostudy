f402e31bafea1e464b187f53b1822b46694dadd4
Mingyu Joung <samu9nai@gmail.com>
2026-05-08 09:22:05 +0900

    [level 0] Title: 옹알이 (1), Time: 3.78 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/Lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        return (int) Arrays.stream(babbling)
+                           .map(s -> s.replaceAll("(aya|ye|woo|ma)", ""))
+                           .filter(s -> s.equals(""))
+                           .count();
+    }
+}
